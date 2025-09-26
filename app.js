e96df0517e74dec6903e4380cc5023026c24ce3b
Rishita seal <pixie166666@gmail.com>
2025-09-26 23:57:33 +0530

    myupdates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/BookAStay";
 main()
@@ -15,14 +16,16 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 };
 
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname, "views"));
+
 app.get("/", (req,res) => {
     res.send("hi, i am root");
 });
 
-app.get("/listings", (req,res) => {
-    Listing.find({}).then((res) => {
-        console.log(res);
-    });
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs",{allListings});
 });
 
 //    app.get("/testListing" , async (req,res) => {
