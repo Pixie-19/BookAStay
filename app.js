8fbedffa2c5ddace03e355f8a7550bb76ed7ad58
Rishita <pixie166666@gmail.com>
2025-11-23 20:42:24 +0530

    updates

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/BookAStay";
 main()
@@ -61,19 +62,13 @@ app.get("/listings/:id", wrapAsync(async (req,res) => {
 //    res.redirect("/listings");
 //});
 app.post("/listings", wrapAsync(async (req, res) => {
-    if(!req.body.listing) {
-        throw new ExpressError(400,"Send Valid Data For Listing");
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if(result.error) {
+        throw new ExpressError(400, result.error);
     }
     const newListing = new Listing(req.body); // ✅ FIXED
-    if(newListing.title) {
-        throw new ExpressError(400,"Title is missing!");
-    }
-    if(newListing.description) {
-        throw new ExpressError(400,"Description is missing!");
-    }
-    if(newListing.location) {
-        throw new ExpressError(400,"Location is missing!");
-    }
+    
     await newListing.save();
     res.redirect("/listings");
 }));
