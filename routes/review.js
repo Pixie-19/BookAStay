16ffb30da559318b23c55216743194b13afb1615
Rishita Seal <pixie166666@gmail.com>
2026-01-21 22:27:54 +0530

    upd

--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema, reviewSchema} = require("../schema.js");
+const {reviewSchema} = require("../schema.js");
 const Review = require("../models/review.js");
 
 
