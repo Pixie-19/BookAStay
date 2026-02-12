85fb2b137f5b7d6ea225bb0f18b9d7daf48c67ab
Rishita Seal <pixie166666@gmail.com>
2026-02-12 10:16:10 +0530

    cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,8 +2,34 @@ const express = require('express');
 const app = express();
 const users = require('./routes/user.js');
 const posts = require('./routes/post.js');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser("secretkey")); // Use cookie-parser middleware with a secret key for signed cookies
+
+app.get("/getsignedcookies", (req, res) => {
+    res.cookie("made-in", "India", {signed: true});
+    res.send("sent you some signed cookies");
+});
+
+app.get("/verify", (req, res) => {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+    res.send("verified");
+});
+
+
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "hello");
+    res.send("sent you some cookies");
+});
+
+app.get("/greet", (req, res) => {
+    const { name = "anonymous" } = req.cookies;
+    res.send(`The cookie says: ${name}`);
+});
 
 app.get('/', (req, res) => {
+    console.log(req.cookies);
     res.send('Welcome to the Classroom Server!');
 });
 
